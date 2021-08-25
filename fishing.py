5a6248a27953505b7989253a1b65b8b077621998
lettuces <wangk408@gmail.com>
2021-08-24 19:41:27 -0700

    added fishing pole and bobber

--- fishing.py
+++ fishing.py
@@ -21,7 +21,6 @@ def load_image(path):
 clock = pygame.time.Clock()
 
 ## MAP---------------------------------------
-
 f = open('./data/map.txt')
 map_data = [[int(c) for c in row] for row in f.read().split('\n')]
 f.close()
@@ -62,27 +61,31 @@ f.close()
 
 ## CLASSES-----------------------------------
 class Cat(pygame.sprite.Sprite):
-    """Represents the cat controlled by the player. Press and hold spacebar to increase throwing distance. Alternate pressing A and L keys to reel in the line. """
-    NET_PULL = 0
-
+    """
+    Represents the cat controlled by the player. 
+    Press and hold spacebar to increase throwing distance. 
+    Alternate pressing left and right arrow keys to reel in the line. 
+    """
     def __init__(self):
         super(Cat, self).__init__()
-        self.color = ['white/', 'black/', 'tabby/']
+        self.color = ['white/', 'siamese/', 'tabby/']
         self.index = 0
         self.images = [load_image(cat_path + self.color[0] + img) for img in os.listdir(cat_path + self.color[0])]
         self.image = self.images[1]
         
         self.rect = pygame.Rect(24,26,24,26)
-        self.rect.center = (160, 80)
+        self.rect.center = (180, 80)
         
     def change_color(self, num):
         self.images = [load_image(cat_path + self.color[num] + img) for img in os.listdir(cat_path + self.color[num])]
         self.image = self.images[0]
 
     def update(self, pressed_keys, event):
-        '''This method iterates through the elements inside self.images and 
-        displays the next one each tick. For a slower animation, you may want to 
-        consider using a timer of some sort so it updates slower.'''
+        '''
+        This method iterates through the elements inside self.images and 
+        displays the next one each tick. 
+        Change the sprite color by pressing 1, 2, or 3.
+        '''
         self.index += 1
         if self.index != 0:
             if self.index > 14:
@@ -91,10 +94,6 @@ class Cat(pygame.sprite.Sprite):
                 self.image = self.images[0]
             else:
                 self.image = self.images[1]
-
-        if pressed_keys[K_RIGHT]:
-            if pressed_keys[K_LEFT]:
-                NET_PULL += 1*REEL_PULL
 
         if pressed_keys[K_1]:
             self.change_color(1)
@@ -105,21 +104,73 @@ class Cat(pygame.sprite.Sprite):
         if pressed_keys[K_3]:
             self.change_color(2)
 
-## GAME LOOP --------------------------------
-running = True
-i = 1
-j = 1
-instructions = 1
-player = Cat()
-group = pygame.sprite.Group(player)
+class Catch(pygame.sprite.Sprite):
+    """ 
+    Represents the items that can be fished up
+    """
+    def __init__(self, type, spawn_rate, pull_strength, points, catch_path):
+        super(Catch, self).__init__()
+        self.type = type
+        self.spawn_rate = spawn_rate
+        self.pull_strength = pull_strength
+        self.points = points
+        self.image = load_image(catch_path)
+        self.time = None
 
-decrease_speed = -1
-power_level = 0
+    def update(self, millisecs):
+        if self.time is not None:  # If the timer has been started...
+            # and 500 ms have elapsed, kill the sprite.
+            if pygame.time.get_ticks() - self.time >= millisecs:
+                self.kill()
+
+
+class Throwable(pygame.sprite.Sprite):
+    """
+    Represents the bobber
+    """
+    def __init__(self):
+        super(Throwable, self).__init__()
+        self.index = 0
+        self.images = [load_image('data/images/bobber/' + img) for img in os.listdir('data/images/bobber')]
+        self.image = self.images[1]
+        print(os.listdir('data/images/bobber'))
+        self.rect = pygame.Rect(15,15,15,15)
+        self.rect.center = (169, 106)
+
+    def update(self, pressed_keys, event):
+        '''
+        This method iterates through the elements inside self.images and 
+        displays the next one each tick. 
+        '''
+        self.index += 1
+        if self.index != 0:
+            if self.index > 24:
+                self.index = 1
+            if self.index < 12:
+                self.image = self.images[0]
+            else:
+                self.image = self.images[1]
+
+## VARIABLES TO BE USED IN GAME LOOP --------------------------------
+running = True
+
+j = 1 #counter for the boat animation
+instructions = 1 #counter for the instructions animation
+
+player = Cat()
+bobbob = Throwable()
+group = pygame.sprite.Group([player, bobbob])
+
+#these variables are for the power bar
+power_level = 5
 bar_x = 25
 bar_y = 25
 bar_height = 10
 x_change = 0
 
+#fish 
+#fish_types = [{type: salmon, }]
+## GAME LOOP --------------------------------
 while running:
     background_surf.fill((240, 255, 255))
     instructions_surf.fill((0,0,0))
@@ -136,20 +187,21 @@ while running:
         if instructions > 40:
             instructions = 1
         instructions += 1
+
     #boat------------------------------------
     if j != 0:
-        if j < 15:
+        if j < 10:
             boat_tile = load_image(boat_path + boat_tiles[0])
-        elif j < 30:
+        elif j < 20:
             boat_tile = load_image(boat_path + boat_tiles[1])
-        elif j < 45:
+        elif j < 30:
             boat_tile = load_image(boat_path + boat_tiles[2])
-        elif j < 60:
+        elif j < 40:
             boat_tile = load_image(boat_path + boat_tiles[3])
         else:
             boat_tile = load_image(boat_path + boat_tiles[4])
 
-    if j > 75:
+    if j > 50:
         j = 1
     j+=1
 
@@ -166,7 +218,7 @@ while running:
             if tile == 3:
                 background_surf.blit(water_tile,(140 + x * 10 - y * 10, 35 + x * 5 + y * 5))   
       
-    
+    #key presses--------------------------------
     pressed_keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -179,28 +231,52 @@ while running:
                 sys.exit()
             if event.key == K_SPACE:
                 instructions = 0
+                power_level = 7
+
   
         if pressed_keys[pygame.K_SPACE]:         
-            x_change = 5
+            x_change = 3
             power_level += x_change
 
         if event.type == KEYUP:
             if event.key == K_SPACE:
                 distance = power_level
-                power_level = 0
+                #enemy.time = pygame.time.get_ticks()
+                
 
-        if power_level > 100:
-            power_level = 0
+        if power_level > 108:
+            power_level = 5
 
-    pygame.draw.rect(instructions_surf, (255,0,0), (bar_x, bar_y, power_level, bar_height))
+        
 
+    #power bar --------------------------------------
+    power_bar_outline = load_image('data/images/power_bar.png')
+    if power_level < 20:
+        power_color = (255,0,0)
+    elif power_level < 50:
+        power_color = (255, 165, 0)
+    elif power_level < 75:
+        power_color = (234, 255, 0)
+    else: 
+        power_color = (50,205,50)
+    
+    #visualize the power bar after instructions disappear
+    if instructions == 0:
+        pygame.draw.rect(instructions_surf, power_color, (bar_x, bar_y, power_level, bar_height), 0, 3)
+        instructions_surf.blit(power_bar_outline,(24,16))
+
+    #fishing pole and bobber ------------------------------------
+    fishing_pole = load_image('data/images/fishing_pole/normal/normal_pole.png')
+    instructions_surf.blit(fishing_pole, (164,59))
+
+    #display images
     group.update(pressed_keys, event)
     group.draw(background_surf)
     
     screen.blit(pygame.transform.scale(background_surf, screen.get_size()), (0,0))
     screen.blit(pygame.transform.scale(instructions_surf, screen.get_size()), (0,0))
 
- 
     pygame.display.flip()
+
     # keeps the game at 30 frames per second
     clock.tick(30)
